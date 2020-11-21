79c3e148324b5fee48da7baa69c17117ad49547a
Turing <turingksc@gmail.com>
2020-11-21 11:10:10 +0800

    .

--- DFAD_mfn.py
+++ DFAD_mfn.py
@@ -68,13 +68,11 @@ def test(args, student, generator, device, test_loader, epoch=0):
 #            if i==0:
 #                vp.add_image( 'input', pack_images( denormalize(data,(0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)).clamp(0,1).detach().cpu().numpy() ) )
 #                vp.add_image( 'generated', pack_images( denormalize(fake,(0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)).clamp(0,1).detach().cpu().numpy() ) )
-            
-            print('==== out', output.size())
-            print('==== target', target.size())
-            if i > 14:
-                print('==== out', F.log_softmax(output, 1))
-                print('==== target', target)
-            test_loss += F.cross_entropy(output, target, reduction='sum').item() # sum up batch loss
+
+            try:
+                test_loss += F.cross_entropy(output, target, reduction='sum').item() # sum up batch loss
+            except:
+                print('=====Exception====', i)
             pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
 
